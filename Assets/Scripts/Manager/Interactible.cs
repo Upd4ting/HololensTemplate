b50bcd40849cc782c81d3fb33fe0b5adf8f1d1fe
Upd4ting <upd4ting@gmail.com>
2018-02-12 11:28:41 +0100

    Import

---
+++ Assets/Scripts/Manager/Interactible.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    // Used like an interface for InteractibleManager
+}
