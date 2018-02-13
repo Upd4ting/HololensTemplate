d9cd25fcf158d0f196f3762539f047bb2721cd23
Floryan Simar <floryansimar@gmail.com>
2018-02-13 14:47:11 +0100

    Mise à jour des gesture, ajout du Drag

---
+++ Assets/Scripts/Manager/Draggable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Draggable : MonoBehaviour
+{
+    public abstract void OnHoloDragEnter();
+    public abstract void OnHoloDragComplete();
+    public abstract void OnHoloDragCancel();
+}
