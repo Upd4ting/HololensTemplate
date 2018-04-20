59a9e58a4af7a555622ad5c9b009cab10ebdef4f
Floryan Simar <floryansimar@gmail.com>
2018-04-20 23:25:46 +0200

    Created tiles

---
+++ Assets/Tile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace HololensTemplate {
+    public class Tile {
+        public const float tilesize = .6f;
+        /// <inheritdoc />
+        public Tile(Vector3 center, bool obstacle = false) {
+            Center   = center;
+            Obstacle = obstacle;
+        }
+
+        public Vector3 Center   { get; } = Vector3.zero;
+        public bool    Obstacle { get; }
+        private GameObject _obj = null;
+        public void Render() {
+            GameObject scene = GameObject.Find("Scene");
+            if (_obj == null) {
+                _obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                _obj.transform.localScale = new Vector3(tilesize / 2 - .01f, .001f, tilesize / 2 - .01f);
+                var v = Center;
+                v.y                    += .5f;
+                _obj.transform.position =  v;
+                if (scene != null)
+                    _obj.transform.parent   =  scene.transform;
+            }
+        }
+    }
+}
